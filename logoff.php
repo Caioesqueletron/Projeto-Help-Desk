119fd11fdb137e4a7c37df344bd364c3cd1faaca
Caioesqueletron <caiocaio620@gmail.com>
2020-10-12 18:41:04 -0300

    Enviando projeto

---
+++ logoff.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+/*
+echo "<pre>";
+print_r($_SESSION);
+echo "<pre>";
+*/
+	//remover indices do array de sessao
+	//unset()
+
+
+	//destruir a variavel de sessao
+	//session_destroy()
+session_destroy();
+header('Location: index.php')
+?>
