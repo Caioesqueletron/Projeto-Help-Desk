2282d699bd9c5ff7740a6b66b55e932674530e2b
Caioesqueletron <caiocaio620@gmail.com>
2020-10-12 18:49:19 -0300

    Adicionando pastas

---
+++ app_help_desk/valida_dado.php
@@ -0,0 +1,75 @@
+<?php
+
+session_start();
+
+// variavel que verfica se a autenticacao foi realizada
+
+$usuario_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
+$perfis = array(1=> 'administrativo', 2=>'usuario');
+
+// usuarios do sistema
+$usuarios_app = array(
+
+	array('id'=> 1,'email' => 'adm@teste.com.br', 'senha' => '123456','perfil_id' => 1),
+
+	array('id'=> 2,'email' => 'user@teste.com.br',
+		'senha'=> '123456','perfil_id' => 1	),
+	array('id'=> 3,'email' => 'jose@teste.com.br','senha'=> '1234','perfil_id' => 2	),
+	array('id'=> 4,'email' => 'maria@teste.com.br',
+		'senha'=> '1234', 'perfil_id' => 2	),
+
+);
+
+foreach ($usuarios_app as $user) {
+	/*
+	echo 'Usuario app:'.  $user['email'] . ' / ' .$user['senha'];
+	echo "<br />";
+	
+	echo 'Usuario form: ' .$_POST['email']. '/' .$_POST['senha'];
+	
+	*/
+	if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha'] ){
+
+		$usuario_autenticado = true;
+		$usuario_id  = $user['id'];
+		$usuario_perfil_id = $user['perfil_id'];
+
+	}
+
+	
+};
+
+if($usuario_autenticado){
+
+		echo "Usuario autenticado";
+		$_SESSION['autenticado'] = 'SIM';
+		$_SESSION['id'] = $usuario_id;
+		$_SESSION['perfil_id'] = $usuario_perfil_id;
+		header('Location: home.php');
+	}else{
+
+		$_SESSION['autenticado'] = 'NAO';
+		header('Location: index.php?login=erro');
+
+		
+
+	}
+
+	echo "<hr>";
+/*
+echo "<pre>";
+print_r($usuarios_app);
+echo "<pre>";
+*/
+/*
+print_r($_POST);
+
+echo "<br>";
+echo $_POST['email'];
+
+echo "<br>";
+ echo $_POST['senha']
+*/
+?>
